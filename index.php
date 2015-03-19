38cbc3e59ba6a176a5b0fb9186115f32236b8e4e
Brandon L Mitchell <brandonlmitchell@Brandons-MacBook-Pro.local>
2015-03-19 10:04:52 -0400

    first commit with index file

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  Created by:      Brandon Mitchell
+ *  Date:            3/18/15
+ *  Last Mod Time:   11:50 PM
+ *  Filename:        index.php
+ */
+
+	//phpinfo();
+
+	echo $_SERVER["USER"] . "<br/>";
+$x = "hiding";
+
+
+	function testFunction($n) {
+		return ($n * $n * $n);
+	}
+
+	$a = array(1,2,3,4,5,6,7);
+	$b = array_map("testFunction",$a);
+	print_r($b);
+	echo $x;
+
+	$x.= " Test";
+	echo $x;
+	//
