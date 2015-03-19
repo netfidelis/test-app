1c9a8ce136ffffc7382a4b81a3ad34db47298f30
Brandon Mitchell <brandon.mitchell@netfidelis.com>
2015-03-19 14:37:09 -0400

    test after form

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *  Created by:      Brandon Mitchell
+ *  Date:            3/19/15
+ *  Last Mod Time:   1:19 PM
+ *  Filename:        functions.php
+ */
+
+
+	function testFunction($n) {
+	return ($n * $n * $n);
+}
