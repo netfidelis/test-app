6499fdf53475c9bd7b44c3feec53a1e2deb777a5
Brandon Mitchell <brandon.mitchell@netfidelis.com>
2015-03-19 14:11:01 -0400

    updated index.php

---
+++ added_in_branchv1/branchv1.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *  Created by:      Brandon Mitchell
+ *  Date:            3/19/15
+ *  Last Mod Time:   11:06 AM
+ *  Filename:        branchv1.php
+ */
+
+echo "Test";
+
+	echo "Edit done from phpStorm";
+	
+	echo "Edit done from github.com";
+
+	echo "Also done from phpstorm";
+
+	echo "2";
